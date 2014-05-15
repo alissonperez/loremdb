4b148e55a37b7ba278f23bba8af43d85071c3a1f
Alisson R. Perez <alissonperez@gmail.com>
2014-05-15 04:15:40 +0000

    Changed table name users to users_test to fail tests in travis

--- tests/mysql-provision.sql
+++ tests/mysql-provision.sql
@@ -1,7 +1,7 @@
 create database if not exists lordb_test ; use lordb_test ;
 
 drop table if exists users ;
-create table users (
+create table users_test (
 	id smallint unsigned not null PRIMARY KEY AUTO_INCREMENT ,
 	name varchar(100) not null ,
 	age tinyint unsigned not null ,
