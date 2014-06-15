8ba5ba7ab5d7e6afcc848d8cebd9826e911436b1
Alisson R. Perez <alissonperez@gmail.com>
2014-06-15 18:23:18 +0000

    Included a 'filter' option to execute a specific module/suite/method test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,14 @@
-from distutils.core import Command
-
+from distutils.cmd import Command
 
 class DiscoverTest(Command):
+    command_name = "Discover Test"
     description = "Discover and run all tests"
-    user_options = []
+    user_options = [
+        ("filter=", "f", "Test filter (see https://docs.python.org/2/library/unittest.html#unittest.TestLoader.loadTestsFromName for more)")
+    ]
 
     def initialize_options(self):
-        pass
+        self.filter = None
 
     def finalize_options(self):
         pass
@@ -15,10 +17,13 @@ class DiscoverTest(Command):
         import unittest
         import os
 
-        project_dir = os.path.dirname(os.path.realpath(__file__))
+        test_loader = unittest.TestLoader()
 
-        test_loader = unittest.defaultTestLoader
-        test_suite = test_loader.discover(project_dir)
+        if self.filter:
+            test_suite = test_loader.loadTestsFromName( "tests." + self.filter ) ;
+        else:
+            project_dir = os.path.dirname(os.path.realpath(__file__))
+            test_suite = test_loader.discover(project_dir)
 
         test_runner = unittest.TextTestRunner()
         if not test_runner.run(test_suite).wasSuccessful():
